e6783da14928bab49a94dccb71a4af3629d73038
Akash Biswas <akash41bt@gmail.com>
2025-10-08 20:21:13 +0600

    insert data to database

---
+++ crud-app/create.php
@@ -0,0 +1,25 @@
+<?php
+include 'dp.php';
+
+if(isset($_POST['submit'])){
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $phone = $_POST['phone'];
+
+    $sql = "insert into users(name, email, phone) values('$name', '$email', '$phone')";
+
+    if($conn->query($sql)){
+        echo "Record Added Succesfully";
+    }
+    else{
+        echo "Error".$conn->error;
+    }
+}
+?>
+
+<form method="post">
+    Name : <input type="text" name = "name"><br>
+    Email: <input type="text" name = "email"><br>
+    Phone: <input type="text" name = "phone"><br>
+    <input type="submit" name = "submit" value="Add User">
+</form>
--- crud-app/dp.php
+++ crud-app/dp.php
@@ -2,7 +2,7 @@
 $host = 'localhost';
 $username = 'root';
 $password = '';
-$dbname = 'crud_app';
+$dbname = 'crud_db';
 
 $conn = mysqli_connect($host, $username, $password, $dbname);
 

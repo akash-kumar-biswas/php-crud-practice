9b85eb06c6d822c1784e59ed93544722a2e029f8
Akash Biswas <akash41bt@gmail.com>
2025-10-08 21:37:17 +0600

    modify

--- crud-app/create.php
+++ crud-app/create.php
@@ -1,5 +1,5 @@
 <?php
-include 'dp.php';
+include 'db.php';
 
 if(isset($_POST['submit'])){
     $name = $_POST['name'];
---
+++ crud-app/db.php
@@ -0,0 +1,13 @@
+<?php
+$host = '127.0.0.1';
+$username = 'root';
+$password = '';
+$dbname = 'crud_db';
+
+$conn = mysqli_connect($host, $username, $password, $dbname);
+
+if (!$conn) {
+    die("Connection failed" . mysqli_connect_error());
+}
+
+?>
